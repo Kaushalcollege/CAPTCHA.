62019641fe705028b66362b3f3b732cee9c39c9b
Y. Kaushal Mohan Kumar <kaushalcollegeacc@gmail.com>
2024-08-29 14:54:43 +0530

    hehe

--- project-root/Differentiation/Akifapp/__init__.py
+++
@@ -1,3 +0,0 @@
-# differentiation_app/__init__.py
-
-default_app_config = 'Akifapp.apps.DifferentiationAppConfig'
--- project-root/Differentiation/UserBehavior/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import MouseMove, KeyPress, Click
-
-admin.site.register(MouseMove)
-admin.site.register(KeyPress)
-admin.site.register(Click)
